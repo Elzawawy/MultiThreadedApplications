57beb6676bcb2ad72d8b913a13da6c4f9128bc88
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-11-20 02:48:57 +0200

    Populated MergeSort FOlder to start working on merge. The MergeSort algorithm implemented in a multi threaded fashion.

---
+++ MergeSort/main.c
@@ -0,0 +1,10 @@
+/***
+ * main.c main application file
+ * Created By : Amr Elzawawy
+ * Created On : 20-11-18
+ ***/
+
+void main(void)
+{
+    
+}
---
+++ MergeSort/mthreadapp.h
@@ -0,0 +1,21 @@
+/*
+* mthreadapp.h Module for interfacing with multi threaded application calls
+* Multi Threaded Matrix multiplication.
+* Multi Threaded Merge sort.
+* Created By : Amr Elzawawy
+* Created On : 17-11-18
+*/
+
+#ifndef M_THREAD_H_
+#define M_THREAD_H_
+
+typedef struct 
+{
+    long** matrix;
+    int rowNum;
+    int columnNum;
+}mthread_Matrix;
+
+void mthread_element_matmul(mthread_Matrix* matrix1, mthread_Matrix* matrix2, mthread_Matrix* matrixResult);
+void mthread_row_matmul(mthread_Matrix* matrix1, mthread_Matrix* matrix2, mthread_Matrix* matrixResult);
+#endif
