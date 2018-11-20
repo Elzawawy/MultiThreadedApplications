be2b86f7877fd4c76e4f10d8dc5656a0c5c00fc5
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-11-20 03:08:13 +0200

    Implemented input reading and got the array and it's size into varaibles. Created the initial form of the merge sort callable method in mthreadapp module.

--- MergeSort/io.h
+++ MergeSort/io.h
@@ -6,7 +6,10 @@
 
 #ifndef IO_H_
 #define IO_H_
+
+//write the relative path of the file from the io.h module.
 #define FILE_NAME "input.txt"
+//maximum length of a line in the file.
 #define MAX_LINE_LENGTH 100
 
 char* readNextLine(void);
--- MergeSort/main.c
+++ MergeSort/main.c
@@ -3,8 +3,34 @@
  * Created By : Amr Elzawawy
  * Created On : 20-11-18
  ***/
-
+#include "io.h"
+#include "parser.h"
+#include <stdlib.h>
+#include <stdio.h>
 void main(void)
 {
+    char *tokens[64];
+    char *line;
+    long arraySize;
+    //read line from file
+    line = readNextLine();
+    //parse line into stream of tokens
+    parseLine(line, tokens);
+    //First line is the size of the array input. 
+    arraySize =strtol(*tokens, NULL, 10);
+    //read line from file
+    line = readNextLine();
+    //parse line into stream of tokens
+    parseLine(line, tokens);
+    //Second line is the array elements. 
+    long inputArray[arraySize];
+    for(int i=0 ; i<arraySize;i++)
+        inputArray[i] = strtol(tokens[i], NULL, 10);
     
+
+    
+
+
+
+
 }
